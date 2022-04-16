2e42868348066ea881fea4389a491ecee043b271
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-04-16 13:41:56 -0700

    Se centra div del spinner

--- js/api.js
+++ js/api.js
@@ -11,21 +11,21 @@ class API {
         const url = `https://api.lyrics.ovh/v1/${this.artista}/${this.cancion}`
         
         mostrarSpinner();
-        fetch(url)
-            .then(response => response.json())
-            .then(resultado => {
-                limpiarHTML();
-                if (resultado.lyrics) {
-                    const { lyrics } = resultado;
-                    UI.divResultado.textContent = lyrics
-                    UI.headingResultado.textContent = `Letra de la cancion ${this.cancion} de ${this.artista}`
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(resultado => {
+        //         limpiarHTML();
+        //         if (resultado.lyrics) {
+        //             const { lyrics } = resultado;
+        //             UI.divResultado.textContent = lyrics
+        //             UI.headingResultado.textContent = `Letra de la cancion ${this.cancion} de ${this.artista}`
                    
-                }else{
-                    mostrarMensaje('La canción no existe, prueba con otra búsqueda');  
-                    UI.headingResultado.textContent = '';                  
-                }
+        //         }else{
+        //             mostrarMensaje('La canción no existe, prueba con otra búsqueda');  
+        //             UI.headingResultado.textContent = '';                  
+        //         }
 
-            });
+        //     });
     }
 }
 
